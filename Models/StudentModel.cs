beb2d398292894219b1ced610b9a859b49cf0f17
wanru_tsai <milankarei1111@gmail.com>
2022-12-20 23:58:14 +0800

    init、ajax

---
+++ Models/StudentModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TEST.Models
+{
+    public class StudentModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,29 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(function () {
+    // 另一種寫法
+    $("#btnSubmit").click(function () {
+        let jsonStr = JSON.stringify({
+            Name: "JOY",
+            Email: "123@gamil.com"
+        });
+        console.log(jsonStr)
+        $.ajax({
+            url: "/home/CreateStudent",
+            type: "POST",
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            data: jsonStr,
+            success: function (result) {
+                $("#txtJsonReturned").val(JSON.stringify(result));
+            },
+            error: function (err) {
+                $("#txtJsonReturned").val("Error while uploading data: \n\n" + err);
+            }
+        });
+    });
+
+
+});
